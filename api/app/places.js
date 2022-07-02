79af0177eab941074095d7096a720d00a6108cdb
Eugene <evgeny.popov.312@gmail.com>
2022-07-02 10:11:14 +0600

    Создал роут для сущности "Place" с методами получения всех сущностей, получения одной сущьности по 'id' и добавления новой сущности заведения. Создал "middleware" "auth" для проверки токена авторизованного пользователя.

---
+++ api/app/places.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const multer = require("multer");
+const config = require("../config");
+const {nanoid} = require("nanoid");
+const path = require("path");
+const Place = require("../models/Place");
+const auth = require("../middleware/auth");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => {
+        cd(null, config.uploadsPath);
+    },
+    filename: (req, file, cd) => {
+        cd(null, nanoid() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const place = await Place.findById({_id: req.params.id});
+
+        return res.send(place);
+
+    } catch (e) {
+        return next(e);
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    try {
+
+        const places = await Place.find();
+
+        return res.send(places);
+
+    } catch (e) {
+        return next(e);
+    }
+});
+
+
+router.post('/', auth, upload.single('image'), async (req, res, next) => {
+    try {
+        if (req.body.agreement === "true") {
+
+            const placeData = {
+                title: req.body.title,
+                user: req.user._id,
+                description: req.body.description,
+                image: null
+            }
+
+            if (req.file){
+                placeData.image = req.file.filename;
+            }
+
+            const place = new Place(placeData);
+            await place.save();
+
+            return res.send({message: 'Added new place in database'});
+        }
+
+        return res.status(400).send({error: "No agreement"});
+
+   } catch (e) {
+       return next(e)
+   }
+});
+
+
+module.exports = router;
